8cbc4c3eea43cd167118bf287f963449a7090e55
Kaike Rocha <kaikerochadev@gmail.com>
2024-01-06 15:22:53 -0300

    Adding a new name to the project

--- nova_tarefa.php
+++ nova_tarefa.php
@@ -2,7 +2,7 @@
 	<head>
 		<meta charset="utf-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<title>App Lista Tarefas</title>
+		<title>TaskMaster</title>
 
 		<link rel="stylesheet" href="css/estilo.css">
 		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
@@ -14,7 +14,7 @@
 			<div class="container">
 				<a class="navbar-brand" href="#">
 					<img src="img/logo.png" width="30" height="30" class="d-inline-block align-top" alt="">
-					App Lista Tarefas
+					TaskMaster
 				</a>
 			</div>
 		</nav>
